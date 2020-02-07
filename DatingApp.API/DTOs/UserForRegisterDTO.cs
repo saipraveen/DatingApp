c8350c0c6929e928aed5146f8222fafd39a6cb94
Saipraveen <saipraveeno@methode.com>
2020-02-07 16:27:57 +0530

    Validation in API

--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
     public class UserForRegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength=4, ErrorMessage="You must specify password between 4 and 10 characters.")]
         public string Password { get; set; }
     }
 }
