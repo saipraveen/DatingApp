aa6a3116f0178f37da91f86a1f70c1b83fb2d4a8
Saipraveen <saipraveeno@methode.com>
2020-02-13 16:14:59 +0530

    navbar, login, WIP Angular services

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully.');
+    }, error => {
+      console.log('Failed to login');
+    });
   }
-
 }
