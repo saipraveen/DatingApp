c8173dca77084028f937f2c888beca74af1732fd
Sai Praveen Oruganti <ossp.ctr@hotmail.com>
2020-02-23 21:56:35 +0530

    Angular JWT library, Ngx Bootstrap

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,28 +10,30 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      // console.log('Logged in successfully.');
-      this.alertify.success('Logged in successfully');
-    }, error => {
-      // console.log(error);
-      this.alertify.error(error);
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success('Logged in successfully');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    // console.log('logged out');
     this.alertify.message('logged out');
   }
 }
